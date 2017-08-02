e389fa95cd420de810c8cd93dabe0edaa80585b7
itlong <itlong@qq.com>
2017-08-02 19:51:41 +0800

    init

---
+++ config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+CONF_MYSQL = {
+    'host'             : '127.0.0.1',
+    'username'         : 'opel',
+    'password'         : 'opel@ssc9',
+    'database'         : 'opel',
+    'port'             : 3306,
+    'charset'          : 'utf8',
+    'tablePrefix'      : '',
+    'raise_on_warnings': True,
+}
