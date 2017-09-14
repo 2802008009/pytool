d91b9815e13572845d4992defc6ec283f3ef5f20
itlong <itlong@qq.com>
2017-09-14 17:03:26 +0800

    git remote add origin https://github.com/2802008009/pytool.git
    Signed-off-by: itlong <itlong@qq.com>

--- nginx_conf/122.10.128.168/ssl_dsyl88/renew_cert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /home/ssl/
-python acme_tiny.py --account-key account.key --csr domain.csr --acme-dir /home/www/challenges/ > signed.crt || exit
-wget -O - https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > intermediate.pem
-cat signed.crt intermediate.pem > chained.pem
-service nginx reload
